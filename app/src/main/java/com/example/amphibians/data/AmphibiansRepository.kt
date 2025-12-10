a0cf55af219b85003648aefac73dd0c63d4c8776
vegads3 <tristannbautista02@gmail.com>
2025-12-09 18:53:25 -0600

    act

--- app/src/main/java/com/example/amphibians/data/AmphibiansRepository.kt
+++ app/src/main/java/com/example/amphibians/data/AmphibiansRepository.kt
@@ -19,11 +19,9 @@ package com.example.amphibians.data
 import com.example.amphibians.model.Amphibian
 import com.example.amphibians.network.AmphibiansApiService
 
-/**
- * Repository retrieves amphibian data from underlying data source.
- */
+
 interface AmphibiansRepository {
-    /** Retrieves list of amphibians from underlying data source */
+
     suspend fun getAmphibians(): List<Amphibian>
 }
 
